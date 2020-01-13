b4a4bc8cf79535454c60e64edac07163c025af4b
ClareCat <clare.curtis@hotmail.com>
2020-01-12 20:01:08 -0800

    Set GEM_PATH and include gemspec files in runfiles (#8)
    * support gems correctly
    
    * Add all gem files no matter how deeply nested

--- ruby/private/bundle/create_bundle_build_file.rb
+++ ruby/private/bundle/create_bundle_build_file.rb
@@ -33,7 +33,8 @@ GEM_TEMPLATE = <<~GEM_TEMPLATE
     name = "{name}",
     srcs = glob(
       include = [
-        "lib/ruby/{ruby_version}/gems/{name}-{version}/**/*",
+        "lib/ruby/{ruby_version}/gems/{name}-{version}/**",
+        "lib/ruby/{ruby_version}/specifications/{name}-{version}.gemspec",
         "bin/*"
       ],
       exclude = {exclude},
